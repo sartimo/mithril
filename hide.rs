1223b41b53d1a5a1d3a17ef05ff45c6ce0bd9d34
sartimo <sartimo10@gmail.com>
2024-03-04 17:30:12 +0100

    working on lightaidra hide

--- hide.rs
+++ hide.rs
@@ -7,4 +7,27 @@ let encodes: [char; 66] = [
   'g', 'a', '+', 'p', '@', 'u', 'n'
 ];
 
+let decodes: [char; 66] = [
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+  'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+  'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+  'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '.', ' '
+]
 
+let encoded[char; 512];
+let decoded[char; 512];
+
+fn encode() {
+  let x = 0;
+  let i = 0;
+  let c = 0;
+  memset(encoded, 0, sizeof(encoded))
+}
+
+fn decode() {
+
+}
+
+fn main() {
+
+}
