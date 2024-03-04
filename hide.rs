c6f6d4b3af59d0baaab6ea9e6d9713ae931ba379
sartimo <sartimo10@gmail.com>
2024-03-04 17:35:10 +0100

    working on lightaidra hide

--- hide.rs
+++ hide.rs
@@ -1,33 +1,63 @@
 //Adapted from lightaidra
+use std::env;
 
-let encodes: [char; 66] = [
-  '%', 'q', '*', 'K', 'C', ')', '&', 'F', '9', '8', 'f', 's', 'r', '2', 't', 'o', '4', 'b', '3',
-  'y', 'i', '_', ':', 'w', 'B', '>', 'z', '=', ';', '!', 'k', '?', '"', 'E', 'A', 'Z', '7', '.',
-  'D', '-', 'm', 'd', '<', 'e', 'x', '5', 'U', '~', 'h', ',', 'j', '|', '$', 'v', '6', 'c', '1',
-  'g', 'a', '+', 'p', '@', 'u', 'n'
+const ENCODES: [char; 64] = [
+    '%', 'q', '*', 'K', 'C', ')', '&', 'F', '9', '8', 'f', 's', 'r', '2', 't', 'o', '4', 'b', '3', 'y',
+    'i', '_', ':', 'w', 'B', '>', 'z', '=', ';', '!', 'k', '?', '"', 'E', 'A', 'Z', '7', '.', 'D', '-',
+    'm', 'd', '<', 'e', 'x', '5', 'U', '~', 'h', ',', 'j', '|', '$', 'v', '6', 'c', '1', 'g', 'a', '+',
+    'p', '@', 'u', 'n',
+];
+const DECODES: [char; 64] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z', '.', ' ',
 ];
 
-let decodes: [char; 66] = [
-  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
-  'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-  'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-  'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '.', ' '
-]
-
-let encoded[char; 512];
-let decoded[char; 512];
-
-fn encode() {
-  let x = 0;
-  let i = 0;
-  let c = 0;
-  memset(encoded, 0, sizeof(encoded))
+fn encode(input: &str) -> String {
+    input
+        .chars()
+        .map(|c| {
+            DECODES
+                .iter()
+                .position(|&x| x == c)
+                .map(|index| ENCODES[index])
+                .unwrap_or(c)
+        })
+        .collect()
 }
 
-fn decode() {
-
+fn decode(input: &str) -> String {
+    input
+        .chars()
+        .map(|c| {
+            ENCODES
+                .iter()
+                .position(|&x| x == c)
+                .map(|index| DECODES[index])
+                .unwrap_or(c)
+        })
+        .collect()
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Usage: ./lighthide [-encode|-decode] [string]");
+        return;
+    }
 
+    match &args[1][..] {
+        "-encode" => {
+            let encoded = encode(&args[2]);
+            let decoded = decode(&encoded);
+            println!("encoded[{}]:\n{}", decoded, encoded);
+        }
+        "-decode" => {
+            let decoded = decode(&args[2]);
+            let encoded = encode(&decoded);
+            println!("decoded[{}]:\n{}", &args[2], decoded);
+        }
+        _ => println!("Usage: ./lighthide [-encode|-decode] [string]"),
+    }
 }
